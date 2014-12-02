67cd92321d7e2a5984358d73227cbf934fd89c0e
yassineS <yassinesouilmi@gmail.com>
2014-12-02 11:29:12 -0500

    Adding tools (not complete)

--- tools.py
+++ tools.py
@@ -23,10 +23,7 @@ cmd_out_vcf = r"""
             cd && /bin/rm -rf $tmpDir;
             """
 
-###################
-# Genomekey tools #
-###################
-
+#######################################
 ## Non-GATK tools ##
 
 # List2input
@@ -42,36 +39,27 @@ class Bam_To_BWA(Tool):
     cpu_req = 4
     mem_req = 12 * 1024
     time_req = 2 * 60
-
     inputs = [inp(format='bam', n='>0')]
     outputs = [out(format='bam'), out(format='bai')]
 
-    class Bam_To_BWA(Tool):
-        name = "BAM to BWA"
-        cpu_req = 4  # orchestra: 4
-        mem_req = 12 * 1024  # max mem used (RSS): 12.4 Gb?
-        time_req = 2 * 60
-        inputs = ['bam']
-        outputs = ['bam', 'bai']
+    def cmd(self, i, s, p):
+        # removed -m MEM option in samtools sort
 
-        def cmd(self, i, s, p):
-            # removed -m MEM option in samtools sort
+        if p['chromosome_only_split']:
+            # Using first readgroup id
 
-            if p['chromosome_only_split']:
-                # Using first readgroup id
-
-                cmd_rg = r"""
+            cmd_rg = r"""
                 rg=$({s[samtools]} view -H {i[bam][0]} | grep -w "@RG" | head -n 1 | sed 's/\t/\\t/g') && echo "RG= $rg";
                 {s[samtools]} view -f 2 -u              {i[bam][0]} {p[prevSn]} > tmpIn.ubam;
                 """
 
-            else:
-                cmd_rg = r"""
+        else:
+            cmd_rg = r"""
                 rg=$({s[samtools]} view -H {i[bam][0]} | grep -w {p[rgId]} | uniq | sed 's/\t/\\t/g') && echo "RG= $rg";
                 {s[samtools]} view -f 2 -u -r {p[rgId]} {i[bam][0]} {p[prevSn]} > tmpIn.ubam;
                 """
 
-            cmd_main = r"""
+        cmd_main = r"""
 	            cp {s[empty_sam]} empty.sam && echo -e $rg >> empty.sam;
                 {s[samtools]} view -u {i[bam][0]} "empty_region" > empty.ubam 2> /dev/null;
 
@@ -91,10 +79,65 @@ class Bam_To_BWA(Tool):
 	            {s[samtools]} index out.bam out.bai;
 
                 """
-            return (cmd_init + cmd_rg + cmd_main + cmd_out)
+        return (cmd_init + cmd_rg + cmd_main + cmd_out)
 
 
-### GATK ###
+# VCF to Annotation
+
+class Vcf2Anno_in(Tool):
+    name = "Convert VCF to Annovar"
+    inputs = ['vcf']
+    outputs = ['anno_in']
+    forward_input = True
+    time_req = 12 * 60
+
+    def cmd(self, i, s, p):
+        return cmd_init + r"""
+
+             {s[annovarext]} vcf2anno '{i[vcf][0]}' > $OUT.anno_in
+
+
+              """
+
+
+# Annovar
+
+class Annotate(Tool):
+    name = "Annotate"
+    inputs = ['anno_in']
+    outputs = ['dir']
+    forward_input = True
+    time_req = 12 * 60
+    mem_req = 12 * 1024
+
+    def cmd(self, i, s, p):
+        return cmd_init + r"""
+
+             {s[annovarext]} anno {p[build]} {p[dbname]} {i[anno_in][0]} $OUT.dir
+
+              """
+
+
+# Merge Annotation
+
+class MergeAnnotations(Tool):
+    name = "Merge Annotations"
+    inputs = ['anno_in', 'dir']
+    outputs = ['dir']
+    mem_req = 40 * 1024
+    time_req = 12 * 60
+    forward_input = True
+
+    def cmd(self, i, s, p):
+        return cmd_init + r"""
+
+              {s[annovarext]} merge {i[anno_in][0]} $OUT.dir {annotated_dir_output}
+
+              """, {'annotated_dir_output': ' '.join(map(str, i['dir']))}
+
+
+#######################################
+## GATK ##
 
 # Indel Realigner
 
@@ -137,9 +180,257 @@ class IndelRealigner(Tool):
         """
 
 
-###################
-# General tools   #
-###################
+class MarkDuplicates(Tool):
+    name = "MarkDuplicates"
+    cpu_req = 2
+    mem_req = 4 * 1024
+    time_req = 2 * 60
+    inputs = ['bam']
+    outputs = ['bam', 'bai', 'metrics']
+
+    def cmd(self, i, s, p):
+        cmd_main = r"""
+
+            {s[java]} -Xmx{self.mem_req}M -jar {s[picard_dir]}/MarkDuplicates.jar
+            TMP_DIR=$tmpDir
+            OUTPUT=$tmpDir/out.bam
+            METRICS_FILE=$tmpDir/out.metrics
+            ASSUME_SORTED=True
+            CREATE_INDEX=True
+            COMPRESSION_LEVEL=0
+            MAX_RECORDS_IN_RAM=1000000
+            VALIDATION_STRINGENCY=SILENT
+            VERBOSITY=INFO
+            {inputs};
+
+            mv -f $tmpDir/out.metrics $OUT.metrics;
+        """
+        return (cmd_init + cmd_main + cmd_out), {'inputs': _list2input(i['bam'], " INPUT=")}
+
+
+class BaseQualityScoreRecalibration(Tool):
+    name = "BQSR"
+    cpu_req = 4
+    mem_req = 12 * 1024
+    time_req = 4 * 60
+    inputs = ['bam']
+    outputs = [TaskFile(name='bam', persist=True), TaskFile(name='bai', persist=True)]
+
+    # no -nt, -nct = 4
+    def cmd(self, i, s, p):
+        cmd_main = r"""
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T BaseRecalibrator
+            -R {s[reference_fasta]}
+            -o $tmpDir/{p[chrom]}.grp
+            -knownSites {s[dbsnp_vcf]}
+            -knownSites {s[1komni_vcf]}
+            -knownSites {s[1kindel_vcf]}
+            -knownSites {s[mills_vcf]}
+            -nct {self.cpu_req}
+            -L {p[chrom]}
+            {inputs};
+
+            printf "\n%s BaseRecalibrator ended\n" "{s[date]}" | tee -a /dev/stderr;
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T PrintReads
+            -R {s[reference_fasta]}
+            -o $tmpDir/out.bam
+            -compress 0
+            -BQSR $tmpDir/{p[chrom]}.grp
+            -nct {self.cpu_req}
+            -L {p[chrom]}
+            {inputs};
+
+            """
+        return (cmd_init + cmd_main + cmd_out), {'inputs': _list2input(i['bam'], "-I ")}
+
+
+# Mean to be used per sample
+class HaplotypeCaller(Tool):
+    name = "HaplotypeCaller"
+    cpu_req = 4
+    mem_req = 16 * 1024
+    time_req = 12 * 60
+    inputs = ['bam']
+    outputs = [TaskFile(name='vcf', persist=True), TaskFile(name='vcf.idx', persist=True)]
+
+    # -nct available
+    def cmd(self, i, s, p):
+        cmd_main = r"""
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T HaplotypeCaller
+            -R {s[reference_fasta]}
+            -D {s[dbsnp_vcf]}
+            -o $tmpDir/out.vcf
+            -pairHMM VECTOR_LOGLESS_CACHING
+            -L {p[chrom]}
+            -nct 1
+            --emitRefConfidence GVCF --variant_index_type LINEAR --variant_index_parameter 128000
+            -A DepthPerAlleleBySample
+            -stand_call_conf 30
+            -stand_emit_conf 10
+            {inputs};
+
+            """
+        return (cmd_init + cmd_main + cmd_out_vcf), {'inputs': _list2input(i['bam'], "-I ")}
+
+
+# Joint Genotyping
+class GenotypeGVCFs(Tool):
+    name = "GenotypeGVCFs"
+    cpu_req = 4
+    mem_req = 12 * 1024
+    time_req = 12 * 60
+    inputs = ['vcf']
+    outputs = ['vcf', 'vcf.idx']
+
+    # -nt available
+    def cmd(self, i, s, p):
+        cmd_main = r"""
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T GenotypeGVCFs
+            -R {s[reference_fasta]}
+            -D {s[dbsnp_vcf]}
+            -o $tmpDir/out.vcf
+            -L {p[chrom]}
+            -nt {self.cpu_req}
+            -A Coverage
+            -A GCContent
+            -A HaplotypeScore
+            -A MappingQualityRankSumTest
+            -A InbreedingCoeff -A FisherStrand -A QualByDepth -A ChromosomeCounts
+            {inputs};
+
+            """
+        return (cmd_init + cmd_main + cmd_out_vcf), {'inputs': _list2input(i['vcf'], "-V ")}
+
+
+class VariantQualityScoreRecalibration(Tool):
+    """
+    VQSR
+    Note that HaplotypeScore is no longer applicable to indels
+    see http://gatkforums.broadinstitute.org/discussion/2463/unified-genotyper-no-haplotype-score-annotated-for-indels
+
+    """
+    name = "VQSR"
+    cpu_req = 4
+    mem_req = 12 * 1024
+    time_req = 12 * 60
+    inputs = ['vcf']
+    outputs = ['vcf', 'vcf.idx', 'R']
+
+
+    # -nt available, -nct not available
+    def cmd(self, i, s, p):
+        """
+        Check gatk forum: http://gatkforums.broadinstitute.org/discussion/1259/what-vqsr-training-sets-arguments-should-i-use-for-my-specific-project
+        --maxGaussians         8 (default), set    1 for small-scale test
+        --minNumBadVariants 1000 (default), set 3000 for small-scale test
+        """
+        cmd_VQSR = r"""
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T VariantRecalibrator
+            -R {s[reference_fasta]}
+            -recalFile    $tmpDir/out.recal
+            -tranchesFile $tmpDir/out.tranches
+            -rscriptFile  $tmpDir/out.R
+            -nt {self.cpu_req}
+            -an MQRankSum -an ReadPosRankSum -an DP -an FS -an QD
+            -mode {p[glm]}
+            -L {p[chrom]}
+            --maxGaussians 1
+            --minNumBadVariants 3000
+            {inputs}
+            """
+        cmd_SNP = r"""
+            -resource:hapmap,known=false,training=true,truth=true,prior=15.0 {s[hapmap_vcf]}
+            -resource:dbsnp,known=true,training=false,truth=false,prior=2.0  {s[dbsnp_vcf]}
+            -resource:omni,known=false,training=true,truth=true,prior=12.0   {s[1komni_vcf]}
+            -resource:1000G,known=false,training=true,truth=false,prior=10.0 {s[1ksnp_vcf]};
+            """
+        cmd_INDEL = r"""
+            -resource:mills,known=false,training=true,truth=true,prior=12.0 {s[mills_vcf]}
+            -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 {s[dbsnp_vcf]};
+            """
+        cmd_apply_VQSR = r"""
+
+            printf "\n%s\n" "{s[date]}" | tee -a /dev/stderr;
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T ApplyRecalibration
+            -R {s[reference_fasta]}
+            -recalFile    $tmpDir/out.recal
+            -tranchesFile $tmpDir/out.tranches
+            -o            $tmpDir/out.vcf
+            --ts_filter_level 99.9
+            -mode {p[glm]}
+            -nt {self.cpu_req}
+            -L {p[chrom]}
+            {inputs};
+
+            mv -f $tmpDir/out.R $OUT.R;
+
+            """
+        if p['glm'] == 'SNP':
+            cmd_rc = cmd_SNP
+        else:
+            cmd_rc = cmd_INDEL
+
+        if p['skip_VQSR']:
+            return " cp {i[vcf][0]} $OUT.vcf; cp {i[vcf][0]}.idx $OUT.vcf.idx; touch $OUT.R"
+        else:
+            return (cmd_init + cmd_VQSR + cmd_rc + cmd_apply_VQSR + cmd_out_vcf), {
+                'inputs': _list2input(i['vcf'], "-input ")}
+
+
+class CombineVariants(Tool):
+    name = "CombineVariants"
+    cpu_req = 4  # max CPU here
+    mem_req = 12 * 1024
+    time_req = 2 * 60
+    inputs = ['vcf']
+    outputs = ['vcf', 'vcf.idx']
+
+    # -nt available, -nct not available
+    # Too many -nt (20?) will cause write error
+    def cmd(self, i, s, p):
+        """
+        :param genotypemergeoptions: select from the following:
+            UNIQUIFY       - Make all sample genotypes unique by file. Each sample shared across RODs gets named sample.ROD.
+            PRIORITIZE     - Take genotypes in priority order (see the priority argument).
+            UNSORTED       - Take the genotypes in any order.
+            REQUIRE_UNIQUE - Require that all samples/genotypes be unique between all inputs.
+        """
+        cmd_main = r"""
+
+            {s[java]} -Djava.io.tmpdir=$tmpDir -Xmx{self.mem_req}M
+            -jar {s[gatk]}
+            -T CombineVariants
+            -R {s[reference_fasta]}
+            -o $tmpDir/out.vcf
+            -genotypeMergeOptions UNSORTED
+            -nt {self.cpu_req}
+            {inputs};
+
+        """
+        return (cmd_init + cmd_main + cmd_out_vcf), {'inputs': _list2input(i['vcf'], "-V ")}
+
+
+#######################################
+## General tools   ##
+
 
 class Sleep(Tool):
     def cmd(self, i, o, time=10):
